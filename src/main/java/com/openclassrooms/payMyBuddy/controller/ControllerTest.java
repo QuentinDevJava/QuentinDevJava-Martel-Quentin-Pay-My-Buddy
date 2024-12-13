d3290e7f60d272997888b625b564726bef47a8fc
QuentinDevJava <168752018+QuentinDevJava@users.noreply.github.com>
2024-12-13 14:36:55 +0100

    Add ControllerTest test.html css file update pom.xml

---
+++ src/main/java/com/openclassrooms/payMyBuddy/controller/ControllerTest.java
@@ -0,0 +1,34 @@
+package com.openclassrooms.payMyBuddy.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.openclassrooms.payMyBuddy.model.Transaction;
+import com.openclassrooms.payMyBuddy.model.User;
+import com.openclassrooms.payMyBuddy.service.TransactionService;
+import com.openclassrooms.payMyBuddy.service.UserService;
+
+@Controller
+@RequestMapping("/")
+public class ControllerTest {
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private TransactionService transactionService;
+
+	@GetMapping
+	public String index(Model model) {
+		List<User> users = userService.getUsers();
+		List<Transaction> transactions = transactionService.getTransactions();
+
+		model.addAttribute("listUser", users);
+		model.addAttribute("listTransaction", transactions);
+
+		return "user";
+	}
+}
